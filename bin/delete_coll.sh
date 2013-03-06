1667c758a1dc3a8f69fa47a1cdafb0724acb7b64
harvestdor-dev lyberadmin <lyberadmin@harvestdor-dev.stanford.edu>
2013-03-06 15:43:46 -0800

    add script for soft_commit and comment shell scripts

--- bin/delete_coll.sh
+++ bin/delete_coll.sh
@@ -1,2 +1,3 @@
 # change value below 
+# this will delete all documents indicated by the query
 curl "https://sul-solr-test.stanford.edu/solr/frda/update?commit=true" -H "Content-Type: application/xml" --data-binary '<delete><query>collection_ssi:Imagesqqq*</query></delete>'
